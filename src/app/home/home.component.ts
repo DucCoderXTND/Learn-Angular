347c2fbc99aad020ac400729e92113344e237b97
DucCoderXT <tranhoangduc2023xt@gmail.com>
2024-02-11 22:02:30 +0700

    Databiding - ngIf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
 
   name = 'Duc Dev';
+  public age = 15;
+  
   constructor(){}
   ngOnInit(): void {
     
